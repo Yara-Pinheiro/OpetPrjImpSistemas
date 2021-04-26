b011f05c348eb42b054668461df3c6f89d346da2
Yara-Pinheiro <71187182+Yara-Pinheiro@users.noreply.github.com>
2021-04-25 21:17:48 -0300

    Delete OpetPrjImpSistemas directory

--- OpetPrjImpSistemas/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-      echo"Bem-Vindo, convidado <br>";
-      echo"<br><a href='login.html'>Faça Login</a><br>";
-      echo"<br>OU<br>";
-      echo"<br><a href='cadastrar.html'> Cadastra-se agora</a>";    
-?>
