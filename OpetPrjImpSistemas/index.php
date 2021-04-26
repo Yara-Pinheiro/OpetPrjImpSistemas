46a7281cf55ca7d6a7df9d29b411c676e8407b6e
Yara-Pinheiro <71187182+Yara-Pinheiro@users.noreply.github.com>
2021-04-25 21:20:32 -0300

    Add files via upload

---
+++ OpetPrjImpSistemas/index.php
@@ -0,0 +1,6 @@
+<?php
+      echo"Bem-Vindo, convidado <br>";
+      echo"<br><a href='login.html'>Faça Login</a><br>";
+      echo"<br>OU<br>";
+      echo"<br><a href='cadastrar.html'> Cadastra-se agora</a>";    
+?>
